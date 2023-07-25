12925b6473764f89df825f638e34d9a661ddc289
alexkobelev <a.p.kobelev@gmial.com>
2023-07-25 22:59:11 +0300

    fix: update timers

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,128 +1,60 @@
 import '../styles/style.scss';
-
-class Time {
-  constructor(counter, max) {
-    this.counter = counter;
-    this.max = max;
-  }
-
-  increase() {
-    this.counter++;
-    if (this.counter === this.max) {
-      this.counter = 0;
-      return true;
-    }
-    return false;
-  }
-
-  formatTime() {
-    return this.counter < 10 ? `0${this.counter}` : this.counter;
-  }
-}
-
-class Timer {
-  constructor() {
-    this.seconds = new Time(0, 60);
-    this.minutes = new Time(0, 60);
-    this.hours = new Time(0, 24);
-
-    this.secondsElement = document.querySelector('.timer__seconds');
-    this.minutesElement = document.querySelector('.timer__minutes');
-    this.hoursElement = document.querySelector('.timer__hours');
-
-    this.startButton = document.querySelector('.timer__start');
-    this.pauseButton = document.querySelector('.timer__pause');
-    this.stopButton = document.querySelector('.timer__stop');
-
-    this.startButton.addEventListener('click', () => {
-      this.start();
-      this.startButton.style.display = 'none';
-      this.pauseButton.style.display = 'block';
-    });
-
-    this.pauseButton.addEventListener('click', () => {
-      this.pause();
-      this.startButton.style.display = 'block';
-      this.pauseButton.style.display = 'none';
-    });
-
-    this.stopButton.addEventListener('click', () => {
-      this.stop();
-      this.startButton.style.display = 'block';
-      this.pauseButton.style.display = 'none';
-    });
-  }
-
-  start() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-    this.interval = setInterval(() => this.updateTimer(), 1000);
-  }
-
-  pause() {
-    clearInterval(this.interval);
-  }
-
-  stop() {
-    clearInterval(this.interval);
-    this.resetTimer();
-  }
-
-  resetTimer() {
-    this.seconds.counter = 0;
-    this.minutes.counter = 0;
-    this.hours.counter = 0;
-
-    this.secondsElement.innerHTML = '';
-    this.minutesElement.innerHTML = '';
-    this.hoursElement.innerHTML = '';
-  }
-
-  updateTimer() {
-    let secCount = this.seconds.increase();
-    this.secondsElement.innerHTML = this.seconds.formatTime();
-
-    if (secCount) {
-      let minCount = this.minutes.increase();
-      this.minutesElement.innerHTML = this.minutes.formatTime() + ':';
-
-      if (minCount) {
-        this.hours.increase();
-        this.hoursElement.innerHTML = this.hours.formatTime() + ':';
-      }
-    }
-  }
-}
+import { Timer } from '../js/timer.js';
 
 let timerId = 0;
 
-document.querySelector('#app').innerHTML = `
+function createPlusButtonHtml() {
+  return `
   <div class="container">
     <div class="add-timer">
       <button class="add-timer__button"></button>
     </div>
   </div>
 `;
+}
 
-let addTimerButton = document.querySelector('.add-timer');
-
-addTimerButton.addEventListener('click', () => {
-  let timerHtmlTemplate = `
-    <div class="timer timer-${timerId}">
-      <div class="timer__time ">
-        <span class="timer__text timer__hours"></span>
-        <span class="timer__text timer__minutes"></span>
-        <span class="timer__text timer__seconds"></span>
-      </div>
-      <div class="timer__buttons">
-        <button class="timer__button timer__start"></button>
-        <button class="timer__button timer__pause"></button>
-        <button class="timer__button timer__stop"></button>
-      </div>
+function createTimerHtml() {
+  return `
+    <div class="timer__time-container ">
+      <span class="timer__time"></span>
+    </div>
+    <div class="timer__buttons">
+      <button class="timer__button timer__start"></button>
+      <button class="timer__button timer__pause"></button>
+      <button class="timer__button timer__stop"></button>
     </div>
   `;
-  addTimerButton.insertAdjacentHTML('beforebegin', timerHtmlTemplate);
-  let timer = new Timer();
+}
+
+function createTimerElement() {
+  const timerElement = document.createElement('div');
+  timerElement.className = `timer timer-${timerId}`;
+  timerElement.innerHTML = createTimerHtml();
+  return timerElement;
+}
+
+function createTimer() {
+  const timerElement = createTimerElement();
+
+  plusButton.parentNode.insertBefore(timerElement, plusButton);
+
+  const timerElements = {
+    time: timerElement.querySelector('.timer__time'),
+    start: timerElement.querySelector('.timer__start'),
+    pause: timerElement.querySelector('.timer__pause'),
+    stop: timerElement.querySelector('.timer__stop'),
+  };
+
+  new Timer(
+    timerElements.time,
+    timerElements.start,
+    timerElements.pause,
+    timerElements.stop
+  );
+
   timerId += 1;
-});
+}
+
+document.querySelector('#app').innerHTML = createPlusButtonHtml();
+const plusButton = document.querySelector('.add-timer');
+plusButton.addEventListener('click', createTimer);
---
+++ src/assets/js/timer.js
@@ -0,0 +1,84 @@
+export class Timer {
+  constructor(timeElement, start, pause, stop) {
+    this.counter = 0;
+    this.timeElement = timeElement;
+    this.startButton = start;
+    this.pauseButton = pause;
+    this.stopButton = stop;
+
+    this.startButton.addEventListener('click', () => {
+      this.startTimer();
+      this.startButton.style.display = 'none';
+      this.pauseButton.style.display = 'block';
+    });
+
+    this.pauseButton.addEventListener('click', () => {
+      this.pauseTimer();
+      this.startButton.style.display = 'block';
+      this.pauseButton.style.display = 'none';
+    });
+
+    this.stopButton.addEventListener('click', () => {
+      this.stopTimer();
+      this.startButton.style.display = 'block';
+      this.pauseButton.style.display = 'none';
+    });
+  }
+
+  get hours() {
+    return this.counter > 3599 ? Math.floor(this.counter / 3600) : '';
+  }
+
+  get minutes() {
+    return this.counter > 59 ? Math.floor(this.counter / 60) % 60 : '';
+  }
+  get seconds() {
+    return this.counter > 59 ? this.counter % 60 : this.counter;
+  }
+
+  formatTime(hours, minutes, seconds) {
+    if (minutes > 0 && seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    if (hours > 0 && minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    if (hours > 0) {
+      return `${hours}:${minutes}:${seconds}`;
+    } else if (minutes > 0) {
+      return `${minutes}:${seconds}`;
+    } else {
+      return `${seconds}`;
+    }
+  }
+
+  updateTimer() {
+    this.counter++;
+    this.timeElement.textContent = this.formatTime(
+      this.hours,
+      this.minutes,
+      this.seconds
+    );
+  }
+
+  startTimer() {
+    if (this.interval) clearInterval(this.interval);
+    this.interval = setInterval(() => this.updateTimer(), 1000);
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  stopTimer() {
+    clearInterval(this.interval);
+    this.resetTimer();
+  }
+
+  resetTimer() {
+    this.counter = 0;
+    this.timeElement.textContent = '';
+  }
+}
